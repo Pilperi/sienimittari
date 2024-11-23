478462c72fec78694b19ee569e62a27dfff85e0f
Pilperi <jouni.dx@gmail.com>
2024-11-23 12:42:32 +0200

    Laita nappimääritykset vastaamaan PCB:tä

--- co2_mittasystem/co2_napit.hpp
+++ co2_mittasystem/co2_napit.hpp
@@ -7,11 +7,14 @@ Nappien operointi
 
 #include <stdint.h>
 
-#define NAPPI_CO2       1<<0
-#define NAPPI_KOSTEUS   1<<1
-#define NAPPI_LAMPOTILA 1<<2
-#define NAPPI_YLOS      1<<3
-#define NAPPI_ALAS      1<<4
+#define NAPPI_YLOS      1<<0
+#define NAPPI_ALAS      1<<1
+#define NAPPI_CTRL      1<<2
+#define NAPPI_LAMPOTILA 1<<3
+#define NAPPI_KOSTEUS   1<<4
+#define NAPPI_CO2       1<<5
+#define NAPPI_OK        1<<6
+#define NAPPI_CANCEL    1<<7
 
 /* Tulkitse miten operointimoodin pitäisi muuttua kun nappia painetaan */
 uint16_t tulkitse_painallus(unsigned char, uint16_t);
